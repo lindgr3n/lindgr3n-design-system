ec431b077453ae73ece04491416ef8dee02a01bc
Johan Lindgren <johan@lindgr3n.com>
2019-10-11 23:07:04 +0200

    Add knobs

--- src/BaseButton.stories.js
+++ src/BaseButton.stories.js
@@ -1,4 +1,9 @@
 import BaseButton from "./BaseButton.vue";
+import { storiesOf } from "@storybook/vue";
+import { withKnobs, boolean, text } from "@storybook/addon-knobs";
+
+const stories = storiesOf("Storybook Knobs", module);
+stories.addDecorator(withKnobs);
 
 export default { title: "Button" };
 
@@ -16,3 +21,23 @@ export const asAComponent = () => ({
   components: { BaseButton },
   template: '<base-button :rounded="true">rounded</base-button>'
 });
+
+/**
+ * Using knobs
+ */
+export const dynamicInput = () => ({
+  components: { BaseButton },
+  props: {
+    isDisabled: {
+      default: boolean("Disabled", false)
+    },
+    text: {
+      default: text("Text", "Hello Storybook")
+    }
+  },
+  template: `<base-button :isDisabled="isDisabled">{{ text }}</base-button>`
+});
+
+dynamicInput.story = {
+  decorators: [withKnobs]
+};
