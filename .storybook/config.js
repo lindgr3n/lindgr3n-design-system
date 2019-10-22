c37aff61082925100dd5edf140eac8a5d0274cc7
Johan Lindgren <johan@lindgr3n.com>
2019-10-22 20:04:29 +0200

    Add global style

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 import { configure, addDecorator } from '@storybook/vue';
 import { withA11y } from "@storybook/addon-a11y";
+import '../src/shared/global.styl';  // Import our global style
 
 addDecorator(withA11y);
 
