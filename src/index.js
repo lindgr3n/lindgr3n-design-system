531561d9ca92a2d9968d79e6f6c9b32c335ded2a
Johan Lindgren <johan@lindgr3n.com>
2019-11-05 22:54:46 +0100

    Export components

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import * as styles from './shared/styles.styl';
+import * as global from './shared/global.styl';
+import * as icons from './shared/icons';
+
+export { styles, global, icons };
+
+// export * from './Avatar';
+import BaseBadge from './BaseBadge.vue';
+import BaseButton from './BaseButton.vue';
+import BaseIcon from './BaseIcon.vue';
+import BaseLink from './BaseLink.vue';
+
+export { BaseBadge, BaseButton, BaseIcon, BaseLink };
